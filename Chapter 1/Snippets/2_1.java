c846b6c4e3dc9df98f62d4d320ba27b6642c1d49
Ian <44680390+IanThaNoob@users.noreply.github.com>
2022-06-10 12:56:50 -0500

    Create 2_1.java

---
+++ Chapter 1/Snippets/2_1.java
@@ -0,0 +1,5 @@
+int int;//doesn't compile
+        Int int;//doesn't compile even if Int class existed
+        int Int;//perfectly fine
+        int
+                Int;//perfectly find, since Java ignores whitespace*/
