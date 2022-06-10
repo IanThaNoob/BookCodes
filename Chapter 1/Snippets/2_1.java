c220dcc4b5fed360546add2c4e69a0ae330ec7e3
Ian <44680390+IanThaNoob@users.noreply.github.com>
2022-06-10 13:51:31 -0500

    Delete 2_1.java

--- Chapter 1/Snippets/2_1.java
+++
@@ -1,5 +0,0 @@
-int int;//doesn't compile
-        Int int;//doesn't compile even if Int class existed
-        int Int;//perfectly fine
-        int
-                Int;//perfectly find, since Java ignores whitespace*/
